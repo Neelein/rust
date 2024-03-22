ab22909034e9a5ab6bd6f6f1642bde54c84d5f68
Neelein <shakya12210305@gmail.com>
2024-03-23 00:24:43 +0800

    create vec

---
+++ Vec/main.rs
@@ -0,0 +1,5 @@
+mod test_vec;
+
+fn main(){
+    test_vec::test_vec_int();
+}
---
+++ Vec/test_vec.rs
@@ -0,0 +1,7 @@
+pub  fn test_vec_int(){
+     let mut my_ints:Vec<i32> = Vec::new();
+
+    my_ints.push(30);
+
+    println!("{:?}",my_ints);
+}
