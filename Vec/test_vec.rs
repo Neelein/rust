a8f86864a3bc249837947ac7da5e779f0de3ac03
Neelein <shakya12210305@gmail.com>
2024-03-24 00:12:14 +0800

    vec print slice element

--- Vec/test_vec.rs
+++ Vec/test_vec.rs
@@ -2,6 +2,11 @@ pub  fn test_vec_int(){
      let mut my_ints:Vec<i32> = Vec::new();
 
     my_ints.push(30);
-
-    println!("{:?}",my_ints);
+    my_ints.push(40);
+    my_ints.push(30);
+    my_ints.push(40);
+    my_ints.push(30);
+    my_ints.push(40);
+    
+    println!("{:?}",&(&my_ints).as_slice()[0..5]);
 }
